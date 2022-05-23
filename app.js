f40df99ff9d521f05c38b49a62f676a26353788e
Léo Bonhême--Morsent <gturyz@gmail.com>
2022-05-23 14:21:06 +0200

    [Section 1] Ajout route POST /api/tasks

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-const task = Joi.object({
-  description: Joi.string().required(),
-  faite: Joi.boolean().required(),
-});
-
 app.get("/api/tasks", (req, res) => {
   res.json(db.getAll());
 });
@@ -18,6 +13,21 @@ app.get("/api/tasks", (req, res) => {
 app.get("/api/task/:id", (req, res) => {
   let id = parseInt(req.params.id);
   res.json(db.memoryDb.get(id));
+});
+
+app.post("/api/tasks", (req, res) => {
+  const payload = req.body;
+
+  const schema = Joi.object({
+    description: Joi.string().required(),
+    faite: Joi.boolean().required(),
+  });
+  const { value, error } = schema.validate(payload);
+  if (error) res.status(400).send({ erreur: error.details[0].message });
+
+  db.insertOne(value);
+
+  res.status(201).json(payload);
 });
 
 module.exports = app;
