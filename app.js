af8adb7625ec72e56d918fd51bf7a9286f3eb2cf
Léo Bonhême--Morsent <gturyz@gmail.com>
2022-05-23 15:39:56 +0200

    [Section 2] Ajout route POST /signup

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 const express = require("express");
+require("express-async-errors");
+
 const Joi = require("joi");
+const bcrypt = require("bcrypt");
 const db = require("./db");
 
 const app = express();
 
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+if (!process.env.JWT_PRIVATE_KEY) {
+  console.log(
+    "Vous devez créer un fichier .env qui contient la variable JWT_PRIVATE_KEY"
+  );
+  process.exit(1);
+}
+
 app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const payload = req.body;
+  const schema = Joi.object({
+    email: Joi.string().required().email(),
+    username: Joi.string().required(),
+    motdepasse: Joi.string().required(),
+  });
+
+  const { value: account, error } = schema.validate(payload);
+  if (error) return res.status(400).send({ erreur: error.details[0].message });
+
+  const { id, found } = db.users.findByProperty("email", account.email);
+  if (found) return res.status(400).send("Please signin instead of signup");
+
+  const salt = await bcrypt.genSalt(10);
+  const passwordHashed = await bcrypt.hash(account.motdepasse, salt);
+  account.motdepasse = passwordHashed;
+
+  db.users.insertOne(account);
+  res.status(201).json({
+    username: account.username,
+    email: account.email,
+  });
+});
 
 app.get("/api/tasks", (req, res) => {
   res.json(db.tasks.getAll());
