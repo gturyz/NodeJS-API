b54c3ebe5073042bfc379277cb62ee392f3246d4
Léo Bonhême--Morsent <gturyz@gmail.com>
2022-05-23 14:07:05 +0200

    [Section 1] Ajout test route GET /api/tasks

---
+++ __tests__/app.spec.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db");
+
+describe("Mon API crud", () => {
+  beforeEach(() => {
+    db.memoryDb = new Map();
+    db.id = 0;
+    db.insertOne({ description: "Alice", faite: false });
+    db.insertOne({ description: "Bob", faite: true });
+    db.insertOne({ description: "Charlie", faite: true });
+  });
+
+  it("GET /api/tasks retourne JSON de la database", async () => {
+    const res = await request(app)
+      .get("/api/tasks")
+      .expect(200)
+      .expect("content-type", /json/);
+    expect(JSON.parse(res.text)).toMatchObject(db.getAll());
+  });
+});
